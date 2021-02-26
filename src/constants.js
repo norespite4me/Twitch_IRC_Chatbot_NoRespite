812cc2d5ab442daedb244490c9af8b1c48dd8163
Interyon <nervetrollz@gmail.com>
2021-02-26 03:32:51 -0500

    rmwarn now untimes user, does not say user had no warnings (idk why it did that so for rmwarn i just took that out entirely, and it does nothing for the if except make sure their warnings can't go below 0)

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,12 @@
-export const CHANNEL_NAME = ['interyon'];
-/*export const CHANNEL_NAME = [
+//export const CHANNEL_NAME = ['interyon'];
+export const CHANNEL_NAME = [
   'interyon', 'razzypls', 'imaefx',
   'leiabelle', 'kindakatt', 'zmalachi',
   'nylawuh', 'bloo', 'annbuuu',
   'krissylin', 'yoitsvortex', 'ads_deadly',
   'iffyiffyiffy_', 'fatherlaur', 'ashencoal',
   'vley', 'bunniejulie'
-];*/
+];
 export const OAUTH_TOKEN = 'oauth:8i5verxh3ardvf2osp3pwkuxqo5heb';
 export const BOT_USERNAME = 'interyon';
 export const BLOCKED_WORDS = [
@@ -14,7 +14,7 @@ export const BLOCKED_WORDS = [
   'kneegrow', 'nigga', 'anal',
   'bugger', 'homo', 'queer',
   'fag', 'l4l', 'f4f',
-  'niggah', 'weewee'
+  'niggah'
 ];
 
 //nwiw7g1k1wza25356ybqv2a9pn7i99
