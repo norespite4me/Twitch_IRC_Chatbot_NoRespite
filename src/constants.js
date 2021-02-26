ea5297ca817b39b710ca6f78dfd6a9426028fde8
Interyon <nervetrollz@gmail.com>
2021-02-26 05:28:58 -0500

    added monty to hosted list

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@ export const CHANNEL_NAME = [
   'nylawuh', 'bloo', 'annbuuu',
   'krissylin', 'yoitsvortex', 'ads_deadly',
   'iffyiffyiffy_', 'fatherlaur', 'ashencoal',
-  'vley', 'bunniejulie'
+  'vley', 'bunniejulie', 'itisnotnormal'
 ];
 export const OAUTH_TOKEN = 'oauth:8i5verxh3ardvf2osp3pwkuxqo5heb';
 export const BOT_USERNAME = 'interyon';
