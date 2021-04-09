cde330bed7b2454a0cdef481f68e1aedab3c2a69
Interyon <nervetrollz@gmail.com>
2021-04-08 22:05:48 -0400

    rewrote some functions and made sure channel_name is correct

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const CHANNEL_NAME = ['itsrft'];
-export const sCHANNEL_NAME = [
+//export const CHANNEL_NAME = ['shortyple'];
+export const CHANNEL_NAME = [
   'interyon', 'imaefx', 'leiabelle',
   'kindakatt', 'zmalachi', 'nylawuh',
   'yoitsvortex', 'ashencoal', 'im_vivid',
