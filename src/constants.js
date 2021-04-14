2cd55767889272c0730cff86ce507406c56b8abf
Interyon <nervetrollz@gmail.com>
2021-04-14 06:12:06 -0400

    added givepoints command, rewrote every 500 messages function, added its_gunning to operating list

--- src/constants.js
+++ src/constants.js
@@ -1,20 +1,19 @@
-//export const CHANNEL_NAME = ['shortyple'];
+//export const CHANNEL_NAME = ['im_vivid'];
 export const CHANNEL_NAME = [
   'interyon', 'imaefx', 'leiabelle',
   'kindakatt', 'zmalachi', 'nylawuh',
   'yoitsvortex', 'ashencoal', 'im_vivid',
   'mikeemadness', 'vley', 'exotixlogic',
-  'ashleeamv', 'gothtarget'
+  'ashleeamv', 'gothtarget', 'its_gunning'
 ];
 export const OAUTH_TOKEN = 'oauth:8i5verxh3ardvf2osp3pwkuxqo5heb';
 export const BOT_USERNAME = 'interyon';
 export const BLOCKED_WORDS = [
   'nigger', 'faggot', 'kneeger',
-  'kneegrow', 'nigga', 'anal',
-  'bugger', 'homo', 'queer',
-  'fag', 'l4l', 'f4f',
-  'niggah', 'terrorist', 'retard',
-  'retarded'
+  'kneegrow', 'nigga', 'bugger',
+  'homo', 'queer', 'fag',
+  'l4l', 'f4f', 'niggah',
+  'terrorist', 'retard', 'retarded'
 ];
 
 //nwiw7g1k1wza25356ybqv2a9pn7i99
