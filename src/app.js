806ca847a0e99e79957ea2cc7a096625f45b6bd1
Interyon <origamist01@gmail.com>
2021-05-16 20:26:40 -0400

    took out auto-shoutout for raids

--- src/app.js
+++ src/app.js
@@ -120,9 +120,6 @@ function onRaidedHandler(channel, username, viewers){
   client.say(channel,
     `/me interyPOP A RAID OF ${viewers}! interyPOP Thank you ${username} for the RAID!!! leiabeLove`
   );
-  client.say(channel,
-    `/me GO DROP A FOLLOW! https://www.twitch.tv/${username}! PogChamp`
-  );
   console.log(`${channel} was raided by ${username} with ${viewers} viewers`.green);
 }
 
