b81d6d2ed09fce1bc59c7c87d7330bc2d83626a9
Interyon <origamist01@gmail.com>
2021-05-22 05:36:25 -0400

    was an error with warn command, fixed by changing target.shift to target = target.substring(1)

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
-//export const CHANNEL_NAME = ['itsrft'];
+//export const CHANNEL_NAME = ['lilfoxu'];
 export const CHANNEL_NAME = [
   'interyon', 'imaefx', 'kindakatt',
   'zmalachi', 'nylawuh', 'its_gunning',
   'yoitsvortex', 'ashencoal', 'im_vivid',
   'mikeemadness', 'vley', 'exotixlogic',
   'itsrft', 'gothtarget', 'leiabelle',
-  'uwureborn'
+  'uwureborn', 'lilfoxu'
 ];
 
 export const ANNOUNCE_ENABLED = [
@@ -21,6 +21,9 @@ export const BLOCKED_WORDS = [
   'kneegrow', 'nigga', 'bugger',
   'homo', 'queer', 'fag',
   'l4l', 'f4f', 'niggah',
-  'terrorist', 'retard', 'retarded'
+  'terrorist', 'retard', 'retarded',
+  'tranny', 'coon', 'simp',
+  'chink', 'kike', 'blackie',
+  'blacky', 'zipperhead', 'virgin',
+  'incel' 
 ];
-
