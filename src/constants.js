a03a4277ae4da395d823ef3345252cedfb881831
Interyon <origamist01@gmail.com>
2021-05-22 18:48:32 -0400

    fixed mikees channel name

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ export const CHANNEL_NAME = [
     'interyon', 'imaefx', 'kindakatt',
     'zmalachi', 'nylawuh', 'its_gunning',
     'yoitsvortex', 'ashencoal', 'im_vivid',
-    'mikeemadness', 'vley', 'exotixlogic',
+    'not__mikee', 'vley', 'exotixlogic',
     'itsrft', 'gothtarget', 'leiabelle',
     'uwureborn', 'lilfoxu', 'shortyple'
 ];
